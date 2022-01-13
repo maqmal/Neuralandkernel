47b32f3aaf8e1c434a9319e3dc5bc704acec5ced
aqmal pangestu <pangestuaqmal@gmail.com>
2022-01-13 13:33:54 +0700

    main page completed

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,16 @@
 import React from "react";
-
-const ImageLinkForm = ()=> {
+import './ImageLinkForm.css'
+const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">
-          {'This Magic will detect Face in a picture. Give it a try'}
+        {'This Magic will detect Face in a picture. Give it a try'}
       </p>
-      <div>
-          <input className='f4 pa2 w-40 center' type={'text'}/>
-          <button className="w-10 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+      <div className="center">
+        <div className="center pa4 br3 shadow-5 w-60 form">
+          <input className='f4 pa2 w-70' type='tex' />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rank = ()=>{
+    return(
+        <div className="white f3">
+            {'Your Current Rank is '}
+        </div>
+    )
+}
+
+export default Rank;
