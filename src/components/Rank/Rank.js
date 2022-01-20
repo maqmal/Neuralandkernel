4ea0f5addbb104625ea544fb0a3617cda65d0e45
aqmal pangestu <pangestuaqmal@gmail.com>
2022-01-20 17:52:16 +0700

    clean up text

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,9 +3,9 @@ import React from "react";
 const Rank = ()=>{
     return(
         <div className="white f3">
-            {'Your Current Rank is '}
+            {''}
             <div className="white f1">
-                {'#14'}
+                {''}
             </div>
         </div>
     )
